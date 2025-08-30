20e6f63c37d1b3e672ce766157e2003ce7ecd2c0
xpos <34091290+xpossed-you@users.noreply.github.com>
2025-08-30 17:41:58 +0800

    add internet connection check and just launch the game if not connected

--- mindustry-be-updater.py
+++ mindustry-be-updater.py
@@ -2,60 +2,75 @@ import requests
 from tqdm import tqdm
 import os, subprocess, time
 
-url = "https://api.github.com/repos/anuken/mindustrybuilds/releases/latest"
-response = requests.get(url)
-data = response.json()
+### FUNCTION ###
 
-link_data = data["url"]
-nama_tag = data["tag_name"]
-jumlah_asset = len(data["assets"])
-waktu_publish = data["published_at"]
+def cek_internet():
+    try:
+        requests.get("https://www.google.com", timeout=5)
+        return True
+    except (requests.ConnectionError, requests.Timeout):
+        return False
 
-nama_file_mindustry_skrang = [f for f in os.listdir() if "Mindustry-BE-Desktop" in f and f.endswith(".jar")]
+### EO FUNCTION ###
 
-if nama_tag in nama_file_mindustry_skrang[0]:
-    print("file skarang paling baru, langsung gas bng")
-    
+if not cek_internet():
+    print("Koneksi internet bermasalah, update batal...")
+
 else:
-    print("sabar, mo update bang")
-    for i in data["assets"]:
-        if "Desktop" in i["name"]:
-            nama_file = i["name"]
-            tipe_konten = i["content_type"]
-            ukuran = round((i["size"] / (1024**2)), 2)
-            link_donlod = i["browser_download_url"]
+    url = "https://api.github.com/repos/anuken/mindustrybuilds/releases/latest"
+    response = requests.get(url)
+    data = response.json()
 
-            break
+    link_data = data["url"]
+    nama_tag = data["tag_name"]
+    jumlah_asset = len(data["assets"])
+    waktu_publish = data["published_at"]
 
-    print()
+    nama_file_mindustry_skrang = [f for f in os.listdir() if "Mindustry-BE-Desktop" in f and f.endswith(".jar")]
 
-    print(f"Nama tag: {nama_tag}")
-    print(f"Jumlah asset: {jumlah_asset}")
-    print(f"Waktu publsih: {waktu_publish}")
-    print(f"Link update: {link_data}")
-    print((len(link_data) + 14) * "=")
-    print(f"Nama file: {nama_file}")
-    print(f"Tipe konten: {tipe_konten}")
-    print(f"Ukuran: {ukuran} mb")
-    print(f"Link file: {link_donlod}")
+    if nama_tag in nama_file_mindustry_skrang[0]:
+        print("file skarang paling baru, langsung gas bng")
+        
+    else:
+        print("sabar, mo update bang")
+        for i in data["assets"]:
+            if "Desktop" in i["name"]:
+                nama_file = i["name"]
+                tipe_konten = i["content_type"]
+                ukuran = round((i["size"] / (1024**2)), 2)
+                link_donlod = i["browser_download_url"]
 
-    with requests.get(link_donlod, stream=True) as r:
-        r.raise_for_status()
-        total_size = int(r.headers.get("content-length", 0))
-        block_size = 1024  # 1 KB chunks
+                break
 
-        # Setup progress bar
-        with tqdm(total=total_size, unit="B", unit_scale=True, desc=nama_file) as bar:
-            with open(nama_file, "wb") as f:
-                for chunk in r.iter_content(chunk_size=block_size):
-                    if chunk:
-                        f.write(chunk)
-                        bar.update(len(chunk))
+        print()
 
-    print(f"hasil download: {nama_file}")
+        print(f"Nama tag: {nama_tag}")
+        print(f"Jumlah asset: {jumlah_asset}")
+        print(f"Waktu publsih: {waktu_publish}")
+        print(f"Link update: {link_data}")
+        print((len(link_data) + 14) * "=")
+        print(f"Nama file: {nama_file}")
+        print(f"Tipe konten: {tipe_konten}")
+        print(f"Ukuran: {ukuran} mb")
+        print(f"Link file: {link_donlod}")
 
-    os.remove(nama_file_mindustry_skrang[0])
-    print(f"file yg dihapus: {nama_file_mindustry_skrang[0]}")
+        with requests.get(link_donlod, stream=True) as r:
+            r.raise_for_status()
+            total_size = int(r.headers.get("content-length", 0))
+            block_size = 1024  # 1 KB chunks
+
+            # Setup progress bar
+            with tqdm(total=total_size, unit="B", unit_scale=True, desc=nama_file) as bar:
+                with open(nama_file, "wb") as f:
+                    for chunk in r.iter_content(chunk_size=block_size):
+                        if chunk:
+                            f.write(chunk)
+                            bar.update(len(chunk))
+
+        print(f"hasil download: {nama_file}")
+
+        os.remove(nama_file_mindustry_skrang[0])
+        print(f"file yg dihapus: {nama_file_mindustry_skrang[0]}")
 
 file_mindustry = [f for f in os.listdir() if "Mindustry-BE-Desktop" in f and f.endswith(".jar")]
 if file_mindustry:
